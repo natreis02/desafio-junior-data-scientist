6ae306333a7571ff0bfcf80431fb45f66676fb69
Natália França dos Reis <89528948+natreis02@users.noreply.github.com>
2024-08-30 14:33:13 -0300

    update analise_sql

---
+++ analise_sql.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tableName (
+    Questões	VARCHAR(512),
+    Resultados Numéricos	VARCHAR(512),
+    Respostas descritivas	VARCHAR(512)
+);
+
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 1', '1756', '');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 2', '366', 'Estacionamento Irregular');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 3', 'Campo Grande, Tijuca, Nan', '');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 4', '510', 'Zona Norte');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 5', '73', '');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 6', '42830', '');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 7', '1214', '');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 8', '834, 241,  139', 'Rock in Rio, Carnaval e Reveillon, respectivamente');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 9', '122', 'Rock in Rio');
+INSERT INTO tableName (Questões, Resultados Numéricos, Respostas descritivas) VALUES ('Questão 10', '58.67, 46.33, respectivamente', 'A média diária de \'Perturbação do sossego\' é de aproximadamente 60, menor que a média diária dos chamados, com exceção do Reveillon que tem média próxima a 50.');
